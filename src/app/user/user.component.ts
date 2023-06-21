e5d6b7e64da3a1ed3f4931491567d3bbdf3597ef
MustafaSoycan <mustafa.soycan@outlook.com>
2023-06-21 15:25:08 +0200

    user component updatedoc

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
+import { Firestore, collection, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -11,12 +13,24 @@ import { User } from 'src/models/user.class';
 export class UserComponent {
 
   user = new User();
+  usersCollection: any; // Typisieren Sie dies entsprechend der Firestore-Sammlung
 
+  constructor(public dialog: MatDialog, private firestore: Firestore) {}
 
-  constructor(public dialog: MatDialog){}
+  ngOnInit(): void {
+    this.usersCollection = collection(this.firestore, 'users');
+    updateDoc(this.usersCollection, this.user.toJSON())
+      .then(() => {
+        console.log('User updated successfully!');
+      });
 
-  openDialog(){
-    this.dialog.open(DialogAddUserComponent);
+    // Überwachen von Änderungen im Array
+    this.usersCollection.valueChanges().subscribe((changes: any[]) => {
+      console.log('Array geändert:', changes);
+    });
   }
 
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
